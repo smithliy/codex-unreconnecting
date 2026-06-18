8d315c0d4600d6e8ac2f9953ffaebf37bd22ef3f
smithliy <g92695628maoji@163.com>
2026-06-18 11:45:09 +0800

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -6,4 +6,4 @@ set "WS_PROXY=http://127.0.0.1:7897"
 set "WSS_PROXY=http://127.0.0.1:7897"
 
 taskkill /F /IM Code.exe >nul 2>nul
-start "" "F:\Microsoft VS Code\Code.exe"
+start "" "D:\Microsoft VS Code\Code.exe"
